5f3ef4fccb86b6a3fbccda57ad15f6a337dd9c49
Zebedee Mason <zeb@cyclerouter.co.uk>
2022-01-21 17:21:00 +0000

    Add packing

---
+++ pack.bat
@@ -0,0 +1,8 @@
+dotnet clean
+dotnet restore
+dotnet build -c Release
+dotnet pack .\Bugbear.Core -c Release -o .\nugets
+dotnet pack .\Bugbear.Graph -c Release -o .\nugets
+dotnet pack .\Bugbear.IO -c Release -o .\nugets
+dotnet pack .\Bugbear.Practical -c Release -o .\nugets
+dotnet pack .\Bugbear.TinkerPop -c Release -o .\nugets
