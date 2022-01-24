4907a03088e238ff7659453c7a8d20e9e4d83806
Zebedee Mason <zeb@cyclerouter.co.uk>
2022-01-24 22:33:45 +0000

    Update pack

--- pack.bat
+++ pack.bat
@@ -1,6 +1,7 @@
 dotnet clean
 dotnet restore
 dotnet build -c Release
+dotnet pack .\Bugbear.Gremlin.Net -c Release -o .\nugets
 dotnet pack .\Bugbear.Core -c Release -o .\nugets
 dotnet pack .\Bugbear.Graph -c Release -o .\nugets
 dotnet pack .\Bugbear.IO -c Release -o .\nugets
